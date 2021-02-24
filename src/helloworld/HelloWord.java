d8deebd05f13c3fabe7e944b66fe0c592e38dd4f
MyungHunSong <change1677@naver.com>
2021-02-24 15:12:38 +0900

    main add

--- src/helloworld/HelloWord.java
+++ src/helloworld/HelloWord.java
@@ -1,10 +1,11 @@
 package helloworld;
 
-public class HelloWord {
+public class HelloWord { // 깃 에게 알려주기 위한 정보
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
+		System.out.println("World's and Dancehall");
 	}
 
 }
